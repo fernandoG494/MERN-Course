7595d45298f4c8be8c20fb8f0b5592b56e25e032
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2022-01-08 12:53:55 -0600

    Primeras pruebas en componente GifGridItem

---
+++ 05-gif-expert-app/src/tests/GifGridItem.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+import GifGridItem from '../components/GifGridItem';
+
+describe('GifGridItem component', () => {
+    const title = "title";
+    const url = "url";
+    const wrapper = shallow(<GifGridItem title={title} url= {url} />)
+
+    test('match snapshot', () => {    
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    test('debe tener title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
+
+    test('debe tener img con props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+
+    test('debe tener el className correcto', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect(className.includes('animate__fadeIn')).toBe(true);
+    });
+});
