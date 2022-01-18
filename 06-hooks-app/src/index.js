4c935832598f30bc598b4857e49d01271d780d4c
fernandoG494 <darkogarcia3@gmail.com>
2022-01-17 19:46:26 -0600

    Formulario con custom Hook

--- 06-hooks-app/src/index.js
+++ 06-hooks-app/src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 // import HookApp from './HookApp'
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
-import SimpleForm from './components/02-useEffect/SimpleForm';
+// import SimpleForm from './components/02-useEffect/SimpleForm';
+import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
 
 ReactDOM.render(
   // <HookApp />,
   // <CounterApp />,
   // <CounterWithCustomHook />,
-  <SimpleForm />,
+  <FormWithCustomHook />,
   document.getElementById('root')
 );
