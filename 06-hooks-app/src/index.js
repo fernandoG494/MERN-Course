f8c57e4a66edd2ee1577475e9df4c7d3c0543237
fernandoG494 <darkogarcia3@gmail.com>
2022-01-18 12:05:33 -0600

    useRef - Primer uso

--- 06-hooks-app/src/index.js
+++ 06-hooks-app/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import FocusScreen from './components/04-useRef/FocusScreen';
 // import HookApp from './HookApp'
 // import CounterApp from './components/01-useState/CounterApp';
 // import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
 // import SimpleForm from './components/02-useEffect/SimpleForm';
 // import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
-import MultipleCustomHook from './components/03-examples/MultipleCustomHook';
+// import MultipleCustomHook from './components/03-examples/MultipleCustomHook';
 
+import './index.css';
 
 ReactDOM.render(
   // <HookApp />,
   // <CounterApp />,
   // <CounterWithCustomHook />,
   // <FormWithCustomHook />,
-  <MultipleCustomHook />,
+  // <MultipleCustomHook />,
+  <FocusScreen />,
   document.getElementById('root')
 );
