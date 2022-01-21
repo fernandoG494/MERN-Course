caad29181e8e77bcc207cd167c74c28f32d22e32
fernandoG494 <darkogarcia3@gmail.com>
2022-01-21 15:04:23 -0600

    Cascaron de ToDo's

--- 06-hooks-app/src/components/08-useReducer/TodoApp.js
+++ 06-hooks-app/src/components/08-useReducer/TodoApp.js
@@ -15,14 +15,48 @@ const TodoApp = () => {
     console.log(todos)
     return (
         <div>
-            <h1>ToDo App</h1>
+            <h1>ToDo App (<small>{ todos.length }</small>)</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className='row'>
+                <div className='col-7'>
+                    <ul className='list-group list-group-flush'>
+                        {
+                            todos.map((item, i) => (
+                                <li
+                                    key={item.id}
+                                    className='list-group-item'
+                                >
+                                    <p className='text-center'>{ i + 1 }. { item.desc }</p>
+                                    <button className='btn btn-danger'>
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+
+                <div className='col-5'>
+                    <h4>Agregar tarea</h4>
+                    <hr />
+
+                    <form>
+                        <input
+                            type="text"
+                            name='description'
+                            className='form-control'
+                            placeholder='Aprender...'
+                            autoComplete='off'
+                        />
+                        <button
+                            className='btn btn-outline-primary mt-1 btn-block'
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
