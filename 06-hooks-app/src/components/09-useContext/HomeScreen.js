3b33b13d5e17e0e5d86df27c243f66db32c1add5
fernandoG494 <darkogarcia3@gmail.com>
2022-01-27 14:07:06 -0600

    Router base

---
+++ 06-hooks-app/src/components/09-useContext/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const homeScreen = () => {
+    return (
+        <div>
+            <h1>Home Screen</h1>
+            <hr />
+        </div>
+    );
+};
+
+export default homeScreen;
