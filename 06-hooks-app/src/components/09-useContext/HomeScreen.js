f0ee5af13093dd832bd0b5749a0b3647daab05e3
fernandoG494 <darkogarcia3@gmail.com>
2022-01-28 14:06:35 -0600

    useContext

--- 06-hooks-app/src/components/09-useContext/HomeScreen.js
+++ 06-hooks-app/src/components/09-useContext/HomeScreen.js
@@ -2,13 +2,17 @@ import React, {useContext} from 'react';
 import { UserContext } from './UserContext';
 
 const HomeScreen = () => {
-    const userContext = useContext(UserContext);
-    console.log(userContext);
+    const {user} = useContext(UserContext);
+    console.log(user);
 
     return (
         <div>
             <h1>Home Screen</h1>
             <hr />
+
+            <pre>
+                { JSON.stringify(user, null, 3) }
+            </pre>
         </div>
     );
 };
--- 06-hooks-app/src/components/09-useContext/NavBar.js
+++ 06-hooks-app/src/components/09-useContext/NavBar.js
@@ -8,9 +8,9 @@ const NavBar = () => {
 
             <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
                 <div className='navbar-nav'>
-                    <NavLink exact activeClassName="active" to="/" className='nav-item nav-link active'>Home</NavLink>
-                    <NavLink exact activeClassName="active" to="/about" className='nav-item nav-link'>About</NavLink>
-                    <NavLink exact activeClassName="active" to="/login" className='nav-item nav-link'>Login</NavLink>
+                    <NavLink to="/" className='nav-item nav-link active'>Home</NavLink>
+                    <NavLink to="/about" className='nav-item nav-link'>About</NavLink>
+                    <NavLink to="/login" className='nav-item nav-link'>Login</NavLink>
                 </div>
             </div>
         </nav>
