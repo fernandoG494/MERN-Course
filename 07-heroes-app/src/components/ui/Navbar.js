44eb3e5e7ecaeb5241ccc092bd87086d05f9d5f6
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2022-04-17 16:34:49 -0500

    Login y Logout implementado con replace

--- 07-heroes-app/src/components/ui/Navbar.js
+++ 07-heroes-app/src/components/ui/Navbar.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React from 'react';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
     
     const handleLogout = () => {
         // TODO: por hacer
         console.log("logOut")
+        navigate('/login', {
+            replace: true
+        });
     }
 
     return (
