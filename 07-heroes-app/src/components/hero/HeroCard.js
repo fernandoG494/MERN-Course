803555990aea47349136d3b323f2d981ba10cefa
Fernando <darkogarcia3@gmail.com>
2022-04-18 22:39:37 -0500

    Cards implemented

---
+++ 07-heroes-app/src/components/hero/HeroCard.js
@@ -0,0 +1,51 @@
+import { Link } from 'react-router-dom';
+
+export const HeroCard = ({
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearence,
+    characters,
+}) => {
+    return (
+        <div className="col">
+            <div className="card">
+                <div className="row no-guitters">
+                    <div className="col-md-4">
+                        <img
+                            src={`/assets/${id}.jpg`}
+                            className="card-img"
+                            alt={ superhero }
+                        />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                { superhero }
+                            </h5>
+                            <p className="card-text">
+                                { alter_ego }
+                            </p>
+
+                            {
+                                (alter_ego !== characters) && 
+                                <p className="text-muted">{characters}</p>
+                            }
+
+                            <p className="card-text">
+                                <small className="text-muted">
+                                    {first_appearence}
+                                </small>
+                            </p>
+
+                            <Link to={`/hero/${id}`}>
+                                Más...
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
