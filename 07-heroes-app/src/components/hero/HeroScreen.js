0b5dbc9df4f5e2c727c067a68479e6ac8b49c4fa
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2022-04-20 18:29:21 -0500

    animated components

--- 07-heroes-app/src/components/hero/HeroScreen.js
+++ 07-heroes-app/src/components/hero/HeroScreen.js
@@ -30,7 +30,7 @@ export const HeroScreen = () => {
                 <img
                     src={ imgPath }
                     alt={ superhero }
-                    className="img-thumbnail"
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
                 />
             </div>
 
