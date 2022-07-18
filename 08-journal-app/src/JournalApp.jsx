0fa6556bbec08cc1895e88fca1c8871f0c9ab475
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2022-07-18 10:43:49 -0500

    Routes implemented

--- 08-journal-app/src/JournalApp.jsx
+++ 08-journal-app/src/JournalApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AppRouter from './router/AppRouter'
 
 const JournalApp = () => {
     return (
         <>
-            Hola mundo
+            <AppRouter /> 
         </>
     )
 }
