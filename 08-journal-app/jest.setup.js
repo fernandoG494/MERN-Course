8881ae0637b89b5be45cf5bfa13de546910951d1
Fernando Garcia <fernando_garcia@epam.com>
2022-08-27 23:05:37 -0500

    Jest test config and file upload tests implementation

---
+++ 08-journal-app/jest.setup.js
@@ -0,0 +1,5 @@
+// En caso de necesitar la implementación del FetchAPI
+import 'whatwg-fetch'; // <-- yarn add whatwg-fetch
+import 'setimmediate';
+
+jest.setTimeout(30000); // To run the long-time tests
---
+++ 08-journal-app/src/tests/helpers/fileUpload.test.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { fileUpload } from '../../helpers/fileUpload';
+
+cloudinary.config({
+    cloud_name: 'dkbliyvln',
+    api_key: '559143325757762',
+    api_secret: 'CiGW8TIEhy6wgBOSbv6r7FW1Fis',
+    secure: true
+});
+
+describe('Pruebas en fileUpload.js', () => {
+    test('debe subir el archivo correctamente a cloudinary', async() => {
+        const imageUrl = 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80';
+        const response = await fetch( imageUrl );
+        const blob = await response.blob();
+        const file = new File([blob], 'foto.jpg');
+
+        const url = await fileUpload(file);
+        expect(typeof url ).toBe('string');
+
+        // console.log(imageId);
+        const segments = url.split('/');
+        const imageId = segments[segments.length - 1].replace('.jpg', '');
+        
+        const cloudResponse = await cloudinary.api.delete_resources([ 'journal-app/' + imageId ], {
+            resource_type: 'image'
+        });
+        console.log({ cloudResponse });
+    });
+
+    test('debe de retornar null', async() => {
+        const file = new File([], 'file.jpg');
+
+        const url = await fileUpload(file);
+        expect( url ).toBe(null);
+    });
+});
