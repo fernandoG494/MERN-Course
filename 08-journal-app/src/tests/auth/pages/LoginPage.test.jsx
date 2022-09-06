433dc59513b65553c2e1283a6c03f2977e3b1016
Fernando Garcia <fernando_garcia@epam.com>
2022-09-06 15:41:58 -0500

    Final test update

---
+++ 08-journal-app/src/tests/auth/pages/LoginPage.test.jsx
@@ -0,0 +1,44 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { LoginPage } from '../../../auth/pages/LoginPage';
+import { authSlice } from '../../../store/auth';
+import { notAuthenticatedState } from '../../fixtures/authFixtures';
+
+const { render, screen, fireEvent } = require("@testing-library/react");
+const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer
+    },
+    preloadedState: {
+        auth: notAuthenticatedState
+    }
+
+});
+
+describe('Pruebas en <LoginPage />', () => {
+    test('debe mostrar el componente correctamente', () => {
+        render(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <LoginPage />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect(screen.getAllByText('Login').length).toBeGreaterThanOrEqual(1);
+    });
+
+    test('boton google debe llamar startGoogleSignIn', () => {
+        render(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <LoginPage />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        const googleBtn = screen.getAllByLabelText('google-btn');
+        fireEvent.click(googleBtn);
+    });
+});
